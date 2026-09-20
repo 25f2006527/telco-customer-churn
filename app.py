460a34e32ba3e7731a4a741c2a97cf5add36887a
Aditya Soni <25f2006527@ds.study.iitm.ac.in>
2026-09-20 23:10:27 +0530

    added app.py, can run on streamlit

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load('churn_model.pkl')
+
+st.title('Customer Churn Prediction')
+st.write('Enter customer details to predict churn likelihood')
+
+st.header('Customer Details')
+
+# Demographics
+gender = st.selectbox('Gender', ['Male', 'Female'])
+senior_citizen = st.selectbox('Senior Citizen', [0, 1])
+partner = st.selectbox('Partner', ['Yes', 'No'])
+dependents = st.selectbox('Dependents', ['Yes', 'No'])
+
+# Account info
+tenure = st.slider('Tenure (months)', 0, 72, 12)
+contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
+paperless_billing = st.selectbox('Paperless Billing', ['Yes', 'No'])
+payment_method = st.selectbox('Payment Method', ['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'])
+monthly_charges = st.number_input('Monthly Charges', 0.0, 200.0, 70.0)
+total_charges = st.number_input('Total Charges', 0.0, 10000.0, 1000.0)
+
+# Services
+phone_service = st.selectbox('Phone Service', ['Yes', 'No'])
+multiple_lines = st.selectbox('Multiple Lines', ['Yes', 'No', 'No phone service'])
+internet_service = st.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'])
+online_security = st.selectbox('Online Security', ['Yes', 'No', 'No internet service'])
+online_backup = st.selectbox('Online Backup', ['Yes', 'No', 'No internet service'])
+device_protection = st.selectbox('Device Protection', ['Yes', 'No', 'No internet service'])
+tech_support = st.selectbox('Tech Support', ['Yes', 'No', 'No internet service'])
+streaming_tv = st.selectbox('Streaming TV', ['Yes', 'No', 'No internet service'])
+streaming_movies = st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet service'])
+
+
+if st.button('Predict Churn'):
+    input_data = pd.DataFrame({
+        'gender': [gender],
+        'SeniorCitizen': [senior_citizen],
+        'Partner': [partner],
+        'Dependents': [dependents],
+        'tenure': [tenure],
+        'PhoneService': [phone_service],
+        'MultipleLines': [multiple_lines],
+        'InternetService': [internet_service],
+        'OnlineSecurity': [online_security],
+        'OnlineBackup': [online_backup],
+        'DeviceProtection': [device_protection],
+        'TechSupport': [tech_support],
+        'StreamingTV': [streaming_tv],
+        'StreamingMovies': [streaming_movies],
+        'Contract': [contract],
+        'PaperlessBilling': [paperless_billing],
+        'PaymentMethod': [payment_method],
+        'MonthlyCharges': [monthly_charges],
+        'TotalCharges': [total_charges]
+    })
+    
+    prediction = model.predict(input_data)[0]
+    probability = model.predict_proba(input_data)[0][1]
+    
+    if prediction == 1:
+        st.error(f'This customer is likely to churn (Probability: {probability:.1%})')
+    else:
+        st.success(f'This customer is likely to stay (Probability of churn: {probability:.1%})')
